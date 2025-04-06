0792ddc51364406d7ff0d253dd3db72067439ae1
Sebastian Ospino <sospino110@gmail.com>
2025-04-06 18:47:51 -0500

    guardar direccion ip correctamente

--- application.py
+++ application.py
@@ -1,9 +1,12 @@
 # application.py
 import os
 from app import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 env_config = os.getenv("FLASK_CONFIG", "config.ProductionConfig")
 application = create_app(config_class=env_config)
+
+application.wsgi_app = ProxyFix(application.wsgi_app, x_for=1, x_proto=1)
 
 if __name__ == "__main__":
     # Only for local dev/testing
